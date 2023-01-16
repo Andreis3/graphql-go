50de45970a66048467ab87b2ac1da7adb931e2c4
andreis3 <andrei.as3@hotmail.com>
2023-01-16 08:46:20 -0300

    first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/andreis3/graphql-go/graph"
+	"github.com/andreis3/graphql-go/internal/database"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatalf("Failed to close the database: %v", err)
+		}
+	}(db)
+
+	categoryDb := database.NewCategory(db)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
